bb72562c3c3970bcc57066e4bb32796b0ba6529b
JulienTom33 <jul.tom@hotmail.fr>
2023-02-20 15:04:47 +0100

    lesson wip

--- lesson_numpy3.py
+++ lesson_numpy3.py
@@ -2,4 +2,18 @@ import numpy as np
 
 np.random.seed(0)
 A = np.random.randint(0, 10, [2, 3])
-print(A)
+# print(A)
+
+# # faire la somme du tableau
+# print(A.sum())
+
+# utiliser la fonction sum sur l'un des axes du tableau
+# l'axe 0 est l'axe vertical, l'axe 1 est l'axe horizontal
+
+# on va faire la somme des éléments qui se suivent horizontalement
+print(A.sum(axis=0))
+
+# on va faire la somme des éléments qqui se suivent verticalement
+print(A.sum(axis=1))
+
+
